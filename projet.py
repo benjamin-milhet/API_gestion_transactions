f8c8a5cfb194cfd1dd8526192689564a3ae46308
clement-gh <64776760+clement-gh@users.noreply.github.com>
2023-01-12 10:47:14 +0100

    Create projet.py

---
+++ projet.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+import datetime
+
+
+
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+	return "Hello,world!"
+
+
+liste_transaction={0: ("jean", "pierre", datetime.date.today(),100)}
+@app.get("/listetransaction")
+def get_list():
+	res =""
+	for i in liste_transaction.items():
+		res+= str(i[1])+"\n"
+	return res
+
+
+'''
+#curl http://127.0.0.1:5000/dico
+
+@app.post("/add")
+def add():
+	mon_dictionnaire[len(mon_dictionnaire)] = request.form["name"]		
+
+	return  str(mon_dictionnaire)
+#curl -X POST -d  "name=tricycle" http://127.0.0.1:5000/add
+@app.post("/del")
+def delete():
+	del mon_dictionnaire[int(request.form["id"])]
+	return str(mon_dictionnaire)
+#curl -X POST -d  "id=2" http://127.0.0.1:5000/del
+
+@app.post("/modif")
+def modif():
+	mon_dictionnaire[int(request.form["id"])] = request.form["name"]		
+	return str(mon_dictionnaire)
+# curl -X POST -d  "id=2" -d "name=truc" http://127.0.0.1:5000/modif
+if __name__ == '__main__':
+    app.run()
+'''
