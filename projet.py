286e78b85dc6eb924daebcee38c7e61bdf0580f7
Benjamin Milhet <benjamin.milhet@outlook.fr>
2023-01-13 09:10:02 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -33,12 +33,12 @@ def add():
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		if sys.argv[1] == "check_syntax":
-			print("Build [OK]")
+			print("Build [ OK ]")
 			exit(0)
 		else:
-			print("Passed argument not supported ! Supported arguments are : check_syntax")
-			exit(1)	
-	app.run()
+			print("Passed argument not supported ! Supported argument : check_syntax")
+			exit(1)
+	app.run(debug=True)
 
 '''
 #curl http://127.0.0.1:5000/dico
