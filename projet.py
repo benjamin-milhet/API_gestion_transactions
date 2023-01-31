b87690ed07ac1d8b4b37c35ff56bf66817741dc8
Benjamin MILHET <benjamin.milhet@outlook.Fr>
2023-01-31 22:44:25 +0100

    Ajout de la date dans le calcul du hash + ajout de commentaires pour chacune des fonctions

--- projet.py
+++ projet.py
@@ -16,23 +16,38 @@ def hello_world():
 
 @app.route("/getTransactions", methods=['GET'])
 def getTransactions():
+    """
+        Renvoie la liste des transactions
+
+        :return: liste des transactions
+    """
     # curl -X GET  http://127.0.0.1:5000/getTransactions
 
-	res = ""
-	for i in sortTransactionsParDate(liste_transaction):
-		res += str(i[1]) + "\n"
-	return res + verifierTransaction()
+    res = ""
+    for i in sortTransactionsParDate(liste_transaction):
+        res += str(i[1]) + "\n"
+    return res + verifierTransaction()
 
 @app.route("/addTransaction", methods=['POST'])
 def add():
+    """
+        Ajoute une transaction
+
+        :param P1: nom de la personne qui donne
+        :param P2: nom de la personne qui reçoit
+        :param s: solde de la transaction
+        
+        :return: liste des transactions
+    """
     # curl -X POST -d "P1=Lucas" -d "P2=Benjamin" -d "s=10" http://127.0.0.1:5000/addTransaction
 
     solde = int(request.form.get("s"))
     P1 = getPersonne(request.form.get("P1"))
     P2 = getPersonne(request.form.get("P2"))
-    h = hashlib.sha256((P1.name + P2.name + str(solde)).encode('utf-8')).hexdigest()
+    date = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    h = hashlib.sha256((P1.name + P2.name + str(solde) + date).encode('utf-8')).hexdigest()
     
-    transaction = (P1.name, P2.name, datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), solde, h)
+    transaction = (P1.name, P2.name, date, solde, h)
     liste_transaction[len(liste_transaction)] = transaction
 
     P1.solde = int(P1.solde) - solde
@@ -45,6 +60,15 @@ def add():
 
 @app.route("/chargerFichier", methods=['POST'])
 def chargerFichierCSV():
+    """
+        Charge les fichiers csv
+
+        :param fichierPersonne: nom du fichier csv des personnes
+        :param fichierTransaction: nom du fichier csv des transactions
+        :param delimiter: délimiteur des fichiers csv
+
+        :return: Le nombre de personne et de transaction chargée
+    """
     # curl -X POST -d "fichierPersonne=data.csv" -d "fichierTransaction=transactions.csv"  -d "delimiter=;" http://127.0.0.1:5000/chargerFichier
 
     resPersonne = chargerFichierPersonne(request.form.get("fichierPersonne"), request.form.get("delimiter"))
@@ -53,57 +77,91 @@ def chargerFichierCSV():
     return resPersonne + resTransaction
 
 def chargerFichierPersonne(fichierPersonne, _delimiter):
-        with open(fichierPersonne, "r") as csvfile:
-                i = 0
-                spamreader = csv.reader(csvfile, delimiter = _delimiter)
-                for row in spamreader:
-                        liste_personne.append(Personne(row[0],row[1]))
-                        i += 1
-        return "Nombre de personne chargée : " + str(i) + "\n"
+    """
+        Charge le fichier csv des personnes
+
+        :param fichierPersonne: nom du fichier csv des personnes
+        :param delimiter: délimiteur des fichiers csv
+
+        :return: Le nombre de personne chargée
+    """
+    with open(fichierPersonne, "r") as csvfile:
+        i = 0
+        spamreader = csv.reader(csvfile, delimiter = _delimiter)
+        for row in spamreader:
+            liste_personne.append(Personne(row[0],row[1]))
+            i += 1
+    return "Nombre de personne chargée : " + str(i) + "\n"
 
 def chargerFichierTransaction(fichierTransaction, _delimiter):
-        with open(fichierTransaction, "r") as csvfile:
-                i = 0
-                spamreader = csv.reader(csvfile, delimiter = _delimiter)
+    """
+        Charge le fichier csv des transactions
 
-                for row in spamreader:
-                        rowP1 = row[0]
-                        rowP2 = row[1]
-                        splitDate = row[2].split(",")
-                        date = datetime.datetime(int(splitDate[0]), int(splitDate[1]), int(splitDate[2]), int(splitDate[3]), int(splitDate[4]), int(splitDate[5]), int(splitDate[6])).strftime("%m/%d/%Y, %H:%M:%S")
-                        s = int(row[3])
-                        h = hashlib.sha256((rowP1 + rowP2 + str(s)).encode('utf-8')).hexdigest()
+        :param fichierTransaction: nom du fichier csv des transactions
 
-                        transaction = (rowP1, rowP2, date, s, h)
-                        liste_transaction[len(liste_transaction)] = transaction
+        :return: Le nombre de transaction chargée
+    """
+    with open(fichierTransaction, "r") as csvfile:
+            i = 0
+            spamreader = csv.reader(csvfile, delimiter = _delimiter)
+
+            for row in spamreader:
+                rowP1 = row[0]
+                rowP2 = row[1]
+                splitDate = row[2].split(",")
+                date = datetime.datetime(int(splitDate[0]), int(splitDate[1]), int(splitDate[2]), int(splitDate[3]), int(splitDate[4]), int(splitDate[5]), int(splitDate[6])).strftime("%m/%d/%Y, %H:%M:%S")
+                s = int(row[3])
+                h = hashlib.sha256((rowP1 + rowP2 + str(s) + date).encode('utf-8')).hexdigest()
+
+                transaction = (rowP1, rowP2, date, s, h)
+                liste_transaction[len(liste_transaction)] = transaction
 			
-                        P1 = getPersonne(rowP1)
-                        P2 = getPersonne(rowP2)
+                P1 = getPersonne(rowP1)
+                P2 = getPersonne(rowP2)
 
-                        P1.solde = int(P1.solde) - s 
-                        P2.solde = int(P2.solde) + s
+                P1.solde = int(P1.solde) - s 
+                P2.solde = int(P2.solde) + s
 			
-                        P1.transactions[len(P1.transactions)] = transaction
-                        P2.transactions[len(P2.transactions)] = transaction
+                P1.transactions[len(P1.transactions)] = transaction
+                P2.transactions[len(P2.transactions)] = transaction
 
-                        i += 1
+                i += 1
 			
-        return "Nombre de transaction chargée : " + str(i) + "\n"
+    return "Nombre de transaction chargée : " + str(i) + "\n"
 
 @app.route("/getPersonne", methods=['GET'])
 def getDataPersonne():
+    """
+        Récupère les données d'une personne
+
+        :param nom: nom de la personne
+
+        :return: les données de la personne (nom, solde, transactions)
+    """
     # curl -X GET -d "nom=Benjamin"  http://127.0.0.1:5000/getPersonne
 
     return str(getPersonne(request.form.get("nom")))
 
 @app.route("/getSoldePersonne", methods=['GET'])
 def getSoldePersonne():
+    """
+        Récupère le solde d'une personne
+
+        :param nom: nom de la personne
+
+        :return: le solde de la personne
+    """
     # curl -X GET -d "nom=Benjamin"  http://127.0.0.1:5000/getSoldePersonne
 
     return str(getPersonne(request.form.get("nom")).solde)
 
 @app.route("/verifierTransaction", methods=['GET'])
 def verifierTransaction():
+    """
+        Vérifie les transactions
+
+        :return: les transactions non valides
+    """
     # curl -X GET http://127.0.0.1:5000/verifierTransaction
 
     res = ""
@@ -114,17 +172,35 @@ def verifierTransaction():
         P1 = getPersonne(transaction[0])
         P2 = getPersonne(transaction[1])
 
-        h = hashlib.sha256((P1.name + P2.name + str(transaction[3])).encode('utf-8')).hexdigest()
+        date = transaction[2]
+
+        h = hashlib.sha256((P1.name + P2.name + str(transaction[3]) + date).encode('utf-8')).hexdigest()
 
         if h != transaction[4]:
             res += str(transaction) + " : " + "Transaction non valide !" + "\n"
 
+        if res == "":
+            res = "Aucune transaction non valide ! \n"
     return res
 
 def sortTransactionsParDate(transactions):
+    """
+        Trie les transactions par date
+
+        :param transactions: les transactions
+
+        :return: les transactions triées par date
+    """
     return sorted(transactions.items(), key=lambda x: x[1][2])
 
 def getPersonne(nom):
+    """
+        Récupère une personne
+
+        :param nom: nom de la personne
+
+        :return: la personne
+    """
     for personne in liste_personne:
         if personne.name == nom:
             return personne
